2aed798eeefd0098f862c2acc44b94e9d2fb5515
Seniru <senirupasan@gmail.com>
2026-04-01 15:02:40 +0530

    refactor: create CodeEditor component for reuse

---
+++ components/CodeEditor.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { onMounted, ref, useTemplateRef } from 'vue';
+import * as monaco from 'monaco-editor'
+
+const editor = useTemplateRef("editor")
+let monacoEditor
+
+const props = defineProps({
+    initialCode: {
+        type: String
+    },
+    actions: {
+        type: [String]
+    },
+
+})
+
+const getText = () => {
+    if (!monacoEditor) return alert("Monaco editor not initialized")
+    return monacoEditor.getValue()
+}
+
+const setText = (text) => {
+    if (!monacoEditor) return alert("Monaco editor not initalized")
+    monacoEditor.setValue(text)
+}
+
+defineExpose({
+    getText,
+    setText
+})
+
+
+onMounted(() => {
+    monacoEditor = monaco.editor.create(editor.value, {
+        language: "javascript",
+        value: props.initialCode,
+        automaticLayout: true,
+        scrollbar: { vertical: 'hidden', horizontal: 'hidden' },
+        minimap: { enabled: false },
+    })
+
+    setText(props.initialCode || "")
+
+    monacoEditor.onKeyDown(ev => {
+        if (
+            ev.code === "Enter" ||
+            ev.code === "Backspace" ||
+            ev.code === "ArrowLeft" ||
+            ev.code === "ArrowRight" ||
+            ev.code === "ArrowUp" ||
+            ev.altKey || ev.metaKey || ev.shiftKey || ev.ctrlKey) return
+        ev.stopPropagation()
+    })
+})
+
+</script>
+
+<template>
+    <div ref="editor" class="min-h-30"></div>
+</template>
--- components/StructureVisualizer.vue
+++ components/StructureVisualizer.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { reactive, useTemplateRef, onMounted, ref } from 'vue'
-import * as monaco from 'monaco-editor'
+import { reactive, useTemplateRef, ref } from 'vue'
+import CodeEditor from './CodeEditor.vue'
 
 const props = defineProps({
   name: {
@@ -12,6 +12,9 @@ const props = defineProps({
   },
   actions: {
     type: [String]
+  },
+  runCode: {
+    type: Function,
   }
 })
 
@@ -19,15 +22,13 @@ const structure = reactive([])
 let editorResult = ref("")
 
 const editor = useTemplateRef("editor")
-let monacoEditor
 
 const runCode = () => {
-  if (!monacoEditor) alert("Monaco editor not initialized")
   try {
     const logs = []
     const originalLog = console.log
     console.log = (...args) => logs.push(args.join(" "))
-    new Function(props.name, monacoEditor.getValue())(structure)
+    new Function(props.name, editor.value.getText())(structure)
     console.log = originalLog
     editorResult.value = logs.join("\n")
   } catch (error) {
@@ -36,48 +37,22 @@ const runCode = () => {
 }
 
 const replaceWithAction = (action) => {
-  if (!monacoEditor) alert("Monaco editor not initialized")
-  monacoEditor.setValue(action)
+  editor.value.setText(action)
 }
 
-onMounted(() => {
-  monacoEditor = monaco.editor.create(editor.value, {
-    language: "javascript",
-    value: props.initialCode,
-    automaticLayout: true,
-    scrollbar: { vertical: 'hidden', horizontal: 'hidden' },
-    minimap: { enabled: false },
-  })
-
-  monacoEditor.onKeyDown(ev => {
-    if (
-      ev.code === "Enter" ||
-      ev.code === "Backspace" ||
-      ev.code === "ArrowLeft" ||
-      ev.code === "ArrowRight" ||
-      ev.code === "ArrowUp" ||
-      ev.code === "Space" ||
-      ev.altKey || ev.metaKey || ev.shiftKey || ev.ctrlKey) return
-    ev.stopPropagation()
-  })
-})
 </script>
 
 <template>
   <div class="flex justify-between gap-5 items-start h-[50vh]">
     <div class="grid container p-4 flex-auto gap-5">
-      <div ref="editor" class="min-h-30"></div>
+      <CodeEditor ref="editor" :initial-code="props.initialCode"/>
       <div class="container">
         <b>Output:</b>
         <div>{{ editorResult }}</div>
       </div>
       <div>
         <button v-on:click="runCode" class="button">Try it out!</button>
-        <button
-          v-for="action in props.actions"
-          class="secondary-button"
-          v-on:click="() => replaceWithAction(action)"
-        >
+        <button v-for="action in props.actions" class="secondary-button" v-on:click="() => replaceWithAction(action)">
           {{ action }}
         </button>
       </div>
@@ -85,10 +60,7 @@ onMounted(() => {
     <div class="container mr-5 flex flex-col-reverse h-full w-max">
       <div class="m-2 text-center">{{ props.name }}</div>
       <div v-if="structure.length">
-        <div
-          v-for="(value, index) in [...structure].reverse()"
-          class="flex items-center gap-2"
-          >
+        <div v-for="(value, index) in [...structure].reverse()" class="flex items-center gap-2">
           <div class="text-gray-500 text-sm w-3 text-right">
             {{ structure.length - index - 1 }}
           </div>
@@ -99,9 +71,9 @@ onMounted(() => {
         </div>
       </div>
       <div v-else class="flex items-center gap-2">
-          <div class="text-gray-500 text-sm w-3 text-right">
-            0
-          </div>
+        <div class="text-gray-500 text-sm w-3 text-right">
+          0
+        </div>
         <div class="w-30 h-12 flex items-center justify-center border border-dashed border-gray-400 text-gray-400">
           Empty
         </div>
